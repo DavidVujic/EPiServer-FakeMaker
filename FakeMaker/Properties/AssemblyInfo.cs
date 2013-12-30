f131c350167c519e61533f7514cd1a07a9d895d4
David Vujic <david@vujic.se>
2013-12-30 22:29:10 +0100

    Updated the assembly version.

--- FakeMaker/Properties/AssemblyInfo.cs
+++ FakeMaker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FakeMaker")]
-[assembly: AssemblyDescription("Help features for test driving EPiServer CMS 7.")]
+[assembly: AssemblyDescription("Help features for test driving EPiServer CMS 7.5")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Vujic")]
 [assembly: AssemblyProduct("FakeMaker")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
