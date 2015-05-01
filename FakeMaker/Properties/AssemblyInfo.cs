94d4adff40f89deda4f823eaf0b980f9d78e568b
David Vujic <david@vujic.se>
2015-05-01 14:49:42 +0200

    Assembly info updates
    Stepped up the version (to be used for creating the Nuget package
    version of FakeMaker).

--- FakeMaker/Properties/AssemblyInfo.cs
+++ FakeMaker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FakeMaker")]
-[assembly: AssemblyDescription("Help features for test driving EPiServer CMS 7.5")]
+[assembly: AssemblyDescription("Help features for test driving EPiServer CMS 8")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Vujic")]
 [assembly: AssemblyProduct("FakeMaker")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
